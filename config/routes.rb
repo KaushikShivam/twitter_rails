70b5b6b906b52f912d3c90e6ccff382e6ed218e1
Shivam Kaushik <shivam.kaushik@outlook.com>
2019-09-04 16:55:33 +0530

    Finish layout form for adding tweets

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   resources :tweets
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  
-  root "tweets#index"
+
+  root 'tweets#index'
 end
